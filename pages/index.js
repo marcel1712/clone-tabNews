08c80e856c26c631cd222078c7f440293f831d26
Marcel Henrique <marcelhrb@gmail.com>
2026-02-10 20:05:42 -0300

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-function Home (){
-    return <h1>Vai Corinthians!! 🖤🦅🤍</h1>
+function Home() {
+  console.log(`<h1> Hello </h1>`);
+  return <h1>Vai Corinthians!! 🖤🦅🤍</h1>;
 }
 
-export default Home
+export default Home;
