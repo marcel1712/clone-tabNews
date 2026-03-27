fdb94ddc104c8fdde3019be7822cd94b3285eb9b
Marcel Henrique <marcelhrb@gmail.com>
2026-03-27 17:52:59 -0300

    adds `Automated Tests` Github Action

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,8 +1,8 @@
-import orchestrator from 'test/orchestrator.js';
+import orchestrator from "test/orchestrator.js";
 
-beforeAll(async() => {
+beforeAll(async () => {
   await orchestrator.waitForAllServices();
-})
+});
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
@@ -14,10 +14,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parseUpdateAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.12");
+  console.log(responseBody.dependencies.database.version);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
 
-test.only("Teste de SQL Injection", async () => {
+test("Teste de SQL Injection", async () => {
   await fetch(
     "http://localhost:3000/api/v1/status?dataBaseName='; SELECT pg_sleep(4); --",
   );
