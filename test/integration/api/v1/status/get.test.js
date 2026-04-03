fc61256caa295af7e05bc80e541f8e5763cb35f7
Marcelhrb <marcelhrb@gmail.com>
2026-04-02 23:21:20 -0300

    test: rename all descriptions to use a new pattern and delete unnecessary console.logs

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -4,20 +4,24 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdateAt);
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+      const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parseUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.12");
-  console.log(responseBody.dependencies.database.version);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.12");
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
 
+// eslint-disable-next-line jest/expect-expect
 test("Teste de SQL Injection", async () => {
   await fetch(
     "http://localhost:3000/api/v1/status?dataBaseName='; SELECT pg_sleep(4); --",
