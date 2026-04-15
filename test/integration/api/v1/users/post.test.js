79637cea15bf46ffac91279b42aac7a38b6187da
Marcel Henrique <marcelhrb@gmail.com>
2026-04-15 16:59:28 -0300

    chore: remove test migration file

---
+++ test/integration/api/v1/users/post.test.js
@@ -0,0 +1,41 @@
+import orchestrator from "test/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.cleanDatabase();
+});
+
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const response1Body = await response1.json();
+
+        expect(response1Body.length).toBeGreaterThan(0);
+        expect(response1.status).toBe(201);
+        expect(Array.isArray(response1Body)).toBe(true);
+      });
+
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const response2Body = await response2.json();
+        expect(response2Body.length).toBe(0);
+        expect(response2.status).toBe(200);
+        expect(Array.isArray(response2Body)).toBe(true);
+      });
+    });
+  });
+});
