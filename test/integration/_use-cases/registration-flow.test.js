a61df1a2568aab81cdb5282cdb9db5fb35c9eace
Marcelhrb <marcelhrb@gmail.com>
2026-05-03 16:34:00 -0300

    feat: send activation email after `user` registration

--- test/integration/_use-cases/registration-flow.test.js
+++ test/integration/_use-cases/registration-flow.test.js
@@ -1,3 +1,4 @@
+import activation from "models/activation.js";
 import orchestrator from "test/orchestrator.js";
 
 beforeAll(async () => {
@@ -8,6 +9,8 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (all successful)", () => {
+  let createUserResponseBody;
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -24,13 +27,13 @@ describe("Use case: Registration Flow (all successful)", () => {
       },
     );
 
-    const createUserResponseBody = await createUserResponse.json();
+    createUserResponseBody = await createUserResponse.json();
 
     expect(createUserResponseBody).toEqual({
       id: createUserResponseBody.id,
       username: "RegistrationFlow",
       email: "registration.flow@feldup.com.br",
-      features:["read:activation_token"],
+      features: ["read:activation_token"],
       password: createUserResponseBody.password,
       created_at: createUserResponseBody.created_at,
       updated_at: createUserResponseBody.updated_at,
@@ -38,14 +41,22 @@ describe("Use case: Registration Flow (all successful)", () => {
   });
 
   test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
 
-  })
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
 
-  test("Activate account", async () => {
+    expect(lastEmail.sender).toBe("<contato@feldup.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@feldup.com.br>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro no Feldup!");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
 
-  })
+    console.log(lastEmail.text);
+  });
 
-  test("Login", async () => {
+  test("Activate account", async () => {});
 
-  })
+  test("Login", async () => {});
 });
