4f60eaa5a59b6e4776eb7394d68bfc0b67da5ef3
Marcelhrb <marcelhrb@gmail.com>
2026-05-04 19:31:06 -0300

    feat: require `read:session` to access `/user` endpoint

--- test/integration/api/v1/user/get.test.js
+++ test/integration/api/v1/user/get.test.js
@@ -16,6 +16,8 @@ describe("GET /api/v1/user", () => {
         username: "UserWithValidSession",
       });
 
+      const activateUser = await orchestrator.activateUser(createdUser);
+
       const sessionObject = await orchestrator.createSession(createdUser.id);
 
       const response = await fetch("http://localhost:3000/api/v1/user", {
@@ -37,10 +39,10 @@ describe("GET /api/v1/user", () => {
         id: createdUser.id,
         username: "UserWithValidSession",
         email: createdUser.email,
-        features:["read:activation_token"],
+        features: ["create:session", "read:session"],
         password: createdUser.password,
         created_at: createdUser.created_at.toISOString(),
-        updated_at: createdUser.updated_at.toISOString(),
+        updated_at: activateUser.updated_at.toISOString(),
       });
 
       expect(uuidVersion(responseBody.id)).toBe(4);
@@ -71,57 +73,6 @@ describe("GET /api/v1/user", () => {
         httpOnly: true,
       });
     });
-
-    // test("With almost expired session", async () => {
-    //   jest.useFakeTimers({
-    //     now: Date.now() - 2505600000,
-    //   });
-
-    //   const createdUser = await orchestrator.createUser({
-    //     username: "UserWithAlmostExpiredSession",
-    //   });
-
-    //   const sessionObject = await orchestrator.createSession(createdUser.id);
-
-    //   jest.useRealTimers();
-
-    //   const response = await fetch("http://localhost:3000/api/v1/user", {
-    //     headers: {
-    //       Cookie: `session_id=${sessionObject.token}`,
-    //     },
-    //   });
-
-    //   expect(response.status).toBe(200);
-
-    //   const responseBody = await response.json();
-    //   console.log(responseBody);
-
-    //   expect(responseBody).toEqual({
-    //     id: createdUser.id,
-    //     username: "UserWithAlmostExpiredSession",
-    //     email: createdUser.email,
-    //     password: createdUser.password,
-    //     created_at: createdUser.created_at.toISOString(),
-    //     updated_at: createdUser.updated_at.toISOString(),
-    //   });
-
-    //   expect(uuidVersion(responseBody.id)).toBe(4);
-    //   expect(Date.parse(responseBody.created_at)).not.toBeNaN();
-    //   expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
-
-    //   //setion renewal assertions
-    //   const renewedSessionObject = await session.findOneValidByToken(
-    //     sessionObject.token,
-    //   );
-
-    //   expect(
-    //     renewedSessionObject.expires_at > sessionObject.expires_at,
-    //   ).toEqual(true);
-    //   expect(
-    //     renewedSessionObject.updated_at > sessionObject.updated_at,
-    //   ).toEqual(true);
-
-    // });
 
     test("With nonexistent session", async () => {
       const nonexistentToken =
@@ -174,6 +125,26 @@ describe("GET /api/v1/user", () => {
         action: "Verifique se este usuario esta logando e tente novamente.",
         status_code: 401,
       });
+    });
+  });
+
+  describe("Anonymous user", () => {
+    test("With valid session", async () => {
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+      });
+
+      expect(response.status).toBe(403);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Voce nao possui permissao para executar",
+        action: `Verifique se o seu usuario possui a feature "read:session"`,
+        status_code: 403,
+      });
+
     });
   });
 });
