274870b10afe97cc3e077207ec4af8796024161b
Marcelhrb <marcelhrb@gmail.com>
2026-05-05 12:56:09 -0300

    feat: consider `resource` in `authorization` model

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,12 +1,14 @@
 import controller from "infra/controller.js";
 import { createRouter } from "next-connect";
 import user from "models/user.js";
+import authorization from "models/authorization.js";
+import { ForbiddenError } from "infra/errors.js";
 
 const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
 router.get(getHandler);
-router.patch(controller.canRequest("update:user"), patchHandler);
+router.patch(controller.canRequest("update:user", ), patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -19,6 +21,17 @@ async function getHandler(request, response) {
 async function patchHandler(request, response) {
   const username = request.query.username;
   const userInputValues = request.body;
+
+  //user, feature, resource
+  const userTryingToPatch = request.context.user;
+  const targetUser = await user.findOneByUsername(username);
+
+  if(!authorization.can(userTryingToPatch, "update:user", targetUser)){
+    throw new ForbiddenError({
+      message: "Voce nao possui permissao para atualizar outro usuario",
+      action: "Verifique se voce possui a feature necessaria para atualizar outro usuario"
+    })
+  }
 
   const updatedUser = await user.update(username, userInputValues);
   return response.status(200).json(updatedUser);
--- test/integration/api/v1/users/[username]/patch.test.js
+++ test/integration/api/v1/users/[username]/patch.test.js
@@ -108,6 +108,46 @@ describe("PATCH /api/v1/users/[username]", () => {
         });
       });
 
+      test("With `userB` targeting `userA`", async () => {
+        await orchestrator.createUser({
+          username: "userA",
+        });
+
+        const createdUserB = await orchestrator.createUser({
+          username: "userB",
+        });
+
+        const activatedUserB = await orchestrator.activateUser(createdUserB);
+        const sessionObject2 = await orchestrator.createSession(
+          activatedUserB.id,
+        );
+
+        const response = await fetch(
+          "http://localhost:3000/api/v1/users/userA",
+          {
+            method: "PATCH",
+            headers: {
+              "Content-type": "application/json",
+              Cookie: `session_id=${sessionObject2.token}`,
+            },
+            body: JSON.stringify({
+              username: "userC",
+            }),
+          },
+        );
+        expect(response.status).toBe(403);
+
+        const responseBody = await response.json();
+
+        expect(responseBody).toEqual({
+          action:
+            "Verifique se voce possui a feature necessaria para atualizar outro usuario",
+          message: "Voce nao possui permissao para atualizar outro usuario",
+          name: "ForbiddenError",
+          status_code: 403,
+        });
+      });
+
       test("With duplicated 'email'", async () => {
         await orchestrator.createUser({
           email: "email1@email.com",
